448be1e4398f412f221d038ba11f65741a7bfcf8
Ash <yukiloh@qq.com>
2019-10-28 12:29:43 +0800

    update;

--- spring-cloud-web-admin-feign/src/main/java/com/test/spring/cloud/web/admin/feign/service/hystrix/AdminServiceHystrix.java
+++ spring-cloud-web-admin-feign/src/main/java/com/test/spring/cloud/web/admin/feign/service/hystrix/AdminServiceHystrix.java
@@ -1,15 +1,37 @@
 package com.test.spring.cloud.web.admin.feign.service.hystrix;
 
+import com.google.common.collect.Lists;
+import com.test.spring.cloud.common.dto.BaseResult;
 import com.test.spring.cloud.web.admin.feign.service.AdminService;
 import org.springframework.stereotype.Component;
 
-
+/*触发熔断后的业务*/
 @Component
 public class AdminServiceHystrix implements AdminService {
     @Override
     public String login(String loginCode, String password) {
-        return String.format("your massage is %s,but request is bad");
+        /*直接返回baseResult*/
+        return BaseResult.notOk(Lists.newArrayList(new BaseResult.Error("502","从上游服务器接收到无效相应")));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*===下方为测试用===*/
 
     @Override
     public String sayHi(String message) {
